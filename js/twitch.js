fe4705e14e19ebab4e133c3bc313a6792a411e87
acummiskey <pleasemailadam@gmail.comgit config --global user.name acummiskeygit config --global user.email pleasemailadam@gmail.com>
2016-06-20 21:09:07 -0400

    js api groundwork

--- js/twitch.js
+++ js/twitch.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
 
 
+var twitchId = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var api = "https://api.twitch.tv/kraken/channels/";
+
+for (i = 0; i < twitchId.length; i++ ) {
+  call = api + twitchId[i];
+  var twit = [];
+  twit = $.getJSON(call);
+
+  console.log(json);
+}
+
+
+
+
+
+
 
 
 
