0b84b386a57e8d3adb001a19a88d77d23f2fa2f2
acummiskey <pleasemailadam@gmail.comgit config --global user.name acummiskeygit config --global user.email pleasemailadam@gmail.com>
2016-06-22 17:29:45 -0400

    JS,CSS,HTML
    Bulk of UI done. Added styling and additional JS for API calls

--- js/twitch.js
+++ js/twitch.js
@@ -3,17 +3,68 @@ $(document).ready(function() {
 
 var twitchId = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 var api = "https://api.twitch.tv/kraken/channels/";
+var streamApi = "https://api.twitch.tv/kraken/streams/";
 
 for (i = 0; i < twitchId.length; i++ ) {
-  call = api + twitchId[i];
-  var twit = [];
-  twit = $.getJSON(call);
+var user = "";
+    user = twitchId[i];
+  call = streamApi + twitchId[i];
 
-  console.log(json);
+getOnline(call, user);
+
+
 }
 
 
+function getOnline(call, user){
 
+  $.getJSON(call, function(ok) {
+  var isOnline = ok.stream;
+
+   if (isOnline) {
+
+   appendDropOn(user);
+
+   }
+   else {
+     appendDropOff(user);
+   }
+
+  });
+
+}
+
+function appendDropOn(user){
+
+$(".dropdown-menu").append("<li id='target'><span class='glyphicon glyphicon-off on' aria-hidden='true'></span> " + user + "</li>");
+
+}
+
+function appendDropOff(user){
+
+$(".dropdown-menu").append("<li class ='liOff disabled' id='target'><span class='glyphicon glyphicon-off off' aria-hidden='true'></span> " + user + "</li>");
+
+}
+
+
+$("ul").click(function(name) {
+$( ".img-responsive" ).remove();
+
+
+  name = $(name.target).text();
+
+  var call = api + name.trim();
+
+  $.getJSON(call, function(ok) {
+
+if (ok.profile_banner) {
+ $(".banner").append("<img src=" + ok.profile_banner +  " class='img-responsive center-block img-rounded' alt='banner' />");
+}
+  });
+
+
+
+});
 
 
 
